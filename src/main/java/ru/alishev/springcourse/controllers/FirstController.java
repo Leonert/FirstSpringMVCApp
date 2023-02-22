b37ea17099b412eba94af2311292d8f2318cbfd2
Leonert <leonid.knyshev@gmail.com>
2023-02-22 13:50:22 +0200

    start using spring servlets

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -3,11 +3,17 @@ package ru.alishev.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String sayHello() {
+    public String sayHello(@RequestParam(value = "name", required = false) String name) {
+        System.out.println(name);
         return "first/hello";
     }
 
