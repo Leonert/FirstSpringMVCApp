0a59ed28d54e154443c6bb5bf8a391630b179e2f
Leonert <leonid.knyshev@gmail.com>
2023-02-22 14:17:42 +0200

    realise basic calculator based on servlets and get requests

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +13,8 @@ import java.io.IOException;
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "name", required = false) String name) {
-        System.out.println(name);
+    public String sayHello(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("message", "Hello" + name);
         return "first/hello";
     }
 
