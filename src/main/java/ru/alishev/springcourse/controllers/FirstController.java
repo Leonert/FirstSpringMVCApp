d7e6e5aa4770ba3ee01ebbc017686acb56fff72d
Leonert <leonid.knyshev@gmail.com>
2023-02-22 12:04:39 +0200

    learn more about controllers

---
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -0,0 +1,18 @@
+package ru.alishev.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String sayGoodBye() {
+        return "first/goodbye";
+    }
+}
